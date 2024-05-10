f4da0d09cabf93ae43722388d3db801cf33b7b93
Jamil18Mendoza <jamilito.khaled@gmail.com>
2024-05-09 21:43:41 -0500

    my first commit

---
+++ drive/Práctica_clase.py
@@ -0,0 +1,26 @@
+# Genere en pantalla un saludo con su nombre. Utilice todos los recursos que dese.
+from symbol import factor
+
+
+print ("Hola, Jamil")
+# Realice un ejemplo de: Suma, Resta, Multiplicación y División
+x = 2
+y = 4
+print (x + y)
+# Utilizando Variables y tipos de datos solicite datos al usuario: NOMBRE, EDAD, ALTURA, PESO
+# luego imprima esos datos en pantalla
+edad = 18
+altura = 1.70
+peso = 55
+print (edad + altura + peso)
+# Se tiene la siguente Lista: frutas = ["manzana", "plátano", "naranja"]
+# Acceder a un elemento de la lista de posición [0])
+# Agregar un elemento a la lista "pera" 
+frutas = ["manzana", "plátano", "naranja"]
+frutas.append("pera")
+print (frutas[0])
+# Se plantea lo siguiente: (Condicionales) 
+# pida un número entero
+# imprima en pantalla si es positivo o negativo 
+
+
