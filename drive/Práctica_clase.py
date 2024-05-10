24743d7df92c4f42ac3caadb8eed42cbe2f08d63
Jamil18Mendoza <jamilito.khaled@gmail.com>
2024-05-09 22:41:10 -0500

    try

--- drive/Práctica_clase.py
+++ drive/Práctica_clase.py
@@ -1,7 +1,4 @@
 # Genere en pantalla un saludo con su nombre. Utilice todos los recursos que dese.
-from symbol import factor
-
-
 print ("Hola, Jamil")
 # Realice un ejemplo de: Suma, Resta, Multiplicación y División
 x = 2
@@ -9,18 +6,29 @@ y = 4
 print (x + y)
 # Utilizando Variables y tipos de datos solicite datos al usuario: NOMBRE, EDAD, ALTURA, PESO
 # luego imprima esos datos en pantalla
-edad = 18
-altura = 1.70
-peso = 55
-print (edad + altura + peso)
+nombre = ("Jamil")
+edad = ("18")
+altura = ("1.70m")
+peso = ("55kg")
+print (nombre)
+print (edad)
+print (altura)
+print (peso)
 # Se tiene la siguente Lista: frutas = ["manzana", "plátano", "naranja"]
 # Acceder a un elemento de la lista de posición [0])
 # Agregar un elemento a la lista "pera" 
 frutas = ["manzana", "plátano", "naranja"]
+primer_fruta = frutas[0]
+print("La primera fruta es:", primer_fruta)
 frutas.append("pera")
-print (frutas[0])
+print("Lista de frutas después de agregar 'pera':", frutas)
 # Se plantea lo siguiente: (Condicionales) 
 # pida un número entero
 # imprima en pantalla si es positivo o negativo 
-
-
+numero = int(input("Por favor, ingresa un número entero: "))
+if numero > 0:
+    print("El número ingresado es positivo.")
+elif numero < 0:
+    print("El número ingresado es negativo.")
+elif numero == 0:
+    print("El número ingresado es cero.")
